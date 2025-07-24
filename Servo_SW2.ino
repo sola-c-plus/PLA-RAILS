88354f3ebabd9911e50c1cf9a06043f5e7a879f3
sola_c_plus <Akinosora.9710@gmail.com>
2025-07-25 08:37:36 +0900

    Add files via upload

---
+++ Servo_SW2.ino
@@ -0,0 +1,33 @@
+#include "arduino_secrets.h"
+
+/*
+  5pin-SW  10pin-Servo data
+
+*/
+#include <Servo.h>
+Servo myServo;
+
+void setup(){
+  myServo.attach(10, 500, 2400);           //10pin servo Data//
+  myServo.write(90);                       //origine--center return//
+  pinMode(5,INPUT_PULLUP);                 //5pin SW 5-GND connect//
+  pinMode(6,INPUT_PULLUP);
+  Serial.begin(9600);
+}
+void loop(){
+  
+  int Push_A = digitalRead(5);
+  int Push_B = digitalRead(6);
+  
+        if ( Push_A ==  LOW ){ 
+          myServo.write( 109 );
+          Serial.println("109");
+        }
+  
+        if ( Push_B ==  LOW ){ 
+          myServo.write( 85 );
+          Serial.println("85");
+        }
+  
+  delay(100);
+}//loop end
