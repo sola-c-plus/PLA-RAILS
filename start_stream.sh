df2ca1706fbc3e19da9227881e15c9f72870f029
sola_c_plus <Akinosora.9710@gmail.com>
2025-08-01 23:03:57 +0900

    Update start_stream.sh

--- start_stream.sh
+++ start_stream.sh
@@ -3,7 +3,7 @@ PORT="8080" #ポート番号
 ID="root" #ID
 PW="password" #パスワード
 SIZE="720x480" #画面サイズ
-FRAMERATE="120" #フレームレート
+FRAMERATE="60" #フレームレート
 export LD_LIBRARY_PATH=/usr/local/lib
 mjpg_streamer \
 -i "input_uvc.so -f $FRAMERATE -r $SIZE -d /dev/video0 -y -n" \
